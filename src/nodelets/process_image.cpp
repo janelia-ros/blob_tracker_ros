11c22cf30b1134d4d73d48a3e577afd15150cde1
Peter Polidoro <peterpolidoro@gmail.com>
2015-09-24 17:03:21 -0400

    Made threshold a dynamic parameter.

--- src/nodelets/process_image.cpp
+++ src/nodelets/process_image.cpp
@@ -82,99 +82,36 @@ void ProcessImageNodelet::connectCb()
 void ProcessImageNodelet::imageCb(const sensor_msgs::ImageConstPtr& image_msg,
                                   const sensor_msgs::CameraInfoConstPtr& info_msg)
 {
-  /// @todo Check image dimensions match info_msg
-  /// @todo Publish tweaks to config_ so they appear in reconfigure_gui
-
   Config config;
   {
     boost::lock_guard<boost::recursive_mutex> lock(config_mutex_);
     config = config_;
   }
-  // int decimation_x = config.decimation_x;
-  // int decimation_y = config.decimation_y;
-
-  // // Compute the ROI we'll actually use
-  // bool is_bayer = sensor_msgs::image_encodings::isBayer(image_msg->encoding);
-  // if (is_bayer)
-  // {
-  //   // Odd offsets for Bayer images basically change the Bayer pattern, but that's
-  //   // unnecessarily complicated to support
-  //   config.x_offset &= ~0x1;
-  //   config.y_offset &= ~0x1;
-  //   config.width &= ~0x1;
-  //   config.height &= ~0x1;
-  // }
-
-  // int max_width = image_msg->width - config.x_offset;
-  // int max_height = image_msg->height - config.y_offset;
-  // int width = config.width;
-  // int height = config.height;
-  // if (width == 0 || width > max_width)
-  //   width = max_width;
-  // if (height == 0 || height > max_height)
-  //   height = max_height;
-
-  // // On no-op, just pass the messages along
-  // if (decimation_x == 1               &&
-  //     decimation_y == 1               &&
-  //     config.x_offset == 0            &&
-  //     config.y_offset == 0            &&
-  //     width  == (int)image_msg->width &&
-  //     height == (int)image_msg->height)
-  // {
-  //   pub_.publish(image_msg, info_msg);
-  //   return;
-  // }
+  int threshold = config.threshold;
 
   // Get a cv::Mat view of the source data
-  cv_bridge::CvImageConstPtr source = cv_bridge::toCvShare(image_msg, sensor_msgs::image_encodings::MONO8);
-
-  // We won't modify the data. We can safely share the data owned by the ROS message instead of copying.
-  // cv_bridge::CvImage output(source->header, source->encoding);
-  // output.image = source->image;
-  // // Apply ROI (no copy, still a view of the image_msg data)
-  // output.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, height));
+  cv_bridge::CvImageConstPtr source_ptr = cv_bridge::toCvShare(image_msg, sensor_msgs::image_encodings::MONO8);
 
   // Threshold
-  cv::Mat image_threshold = cv::Mat::zeros(source.image.size(), source.image.type());
-  cv::threshold(source,image_threshold,100,255,cv::THRESH_BINARY);
+  cv::Mat image_threshold = cv::Mat::zeros(source_ptr->image.size(), source_ptr->image.type());
+  cv::threshold(source_ptr->image,image_threshold,threshold,255,cv::THRESH_BINARY);
 
-  // We want to modify the data in-place. We have to make a copy of the ROS message data.
-  cv_bridge::CvImagePtr output;
-  try
-  {
-    // output = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
-    output = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::MONO8);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+  // Output Image
+  cv::Mat image_output;
+  cv::cvtColor(image_threshold,image_output,CV_GRAY2BGR);
 
-  // Draw an example circle on the video stream
-  output.image = image_threshold.image;
-  // if (output->image.rows > 60 && output->image.cols > 60)
-  //   cv::circle(output->image, cv::Point(512, 512), 20, cv::Scalar(255,0,0), -1);
+  // Draw test circle
+  cv::circle(image_output, cv::Point(512, 512), 50, cv::Scalar(255,255,0), -1);
 
-  // Create output Image message
-  /// @todo Could save copies by allocating this above and having output.image alias it
-  // sensor_msgs::ImagePtr out_image = output.toImageMsg();
-  sensor_msgs::ImagePtr out_image = output->toImageMsg();
+  cv_bridge::CvImage output;
+  output.image = image_output;
+  output.header = source_ptr->header;
+  output.encoding = sensor_msgs::image_encodings::BGR8;
+
+  sensor_msgs::ImagePtr out_image = output.toImageMsg();
 
   // Create updated CameraInfo message
   sensor_msgs::CameraInfoPtr out_info = boost::make_shared<sensor_msgs::CameraInfo>(*info_msg);
-  // int binning_x = std::max((int)info_msg->binning_x, 1);
-  // int binning_y = std::max((int)info_msg->binning_y, 1);
-  // out_info->binning_x = binning_x * config.decimation_x;
-  // out_info->binning_y = binning_y * config.decimation_y;
-  // out_info->roi.x_offset += config.x_offset * binning_x;
-  // out_info->roi.y_offset += config.y_offset * binning_y;
-  // out_info->roi.height = height * binning_y;
-  // out_info->roi.width = width * binning_x;
-  // // If no ROI specified, leave do_rectify as-is. If ROI specified, set do_rectify = true.
-  // if (width != (int)image_msg->width || height != (int)image_msg->height)
-  //   out_info->roi.do_rectify = true;
 
   pub_.publish(out_image, out_info);
 }
